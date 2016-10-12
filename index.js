e4d9beb4b8e034d6313f73d6e4ae3251811db00d
darren wynne <darrenwynne@darrenwynne.local>
2016-10-12 15:27:39 -0500

    fix typo

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ controller.hears(['list'], ['direct_mention','direct_message'], function (bot, m
   bot.reply(message, reply);
 })
 
-controller.hears(['do', 'do @(.*)], ['direct_mention'], function (bot, message) {
+controller.hears(['do', 'do @(.*)'], ['direct_mention'], function (bot, message) {
 /*
   const { user, channel, text } = message;
   const userData = text.match(/<@([A-Z0–9]{9})>/); // parse the text for user's 9 character id
